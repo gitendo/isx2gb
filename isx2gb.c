d38eb483d64514750c37e63669f22ba9a175d27f
gitendo <tmk@tuta.io>
2019-02-11 11:42:13 +0100

    syncing minor changes

--- isx2gb.c
+++ isx2gb.c
@@ -5,6 +5,9 @@
 
 #include "isx2gb.h"
 
+FILE *fp = NULL;
+void *isxp = NULL;
+
 void banner(void)
 {
 	printf("\nisx2gb v%.2f - ISX to Game Boy ROM converter\n", VERSION);
@@ -26,23 +29,35 @@ void usage(void)
 }
 
 
-void error_handler(FILE *ifp, FILE *ofp)
+void handler(char error)
 {
-	if(ifp)
-		fclose(ifp);
-	if(ofp)
-		fclose(ofp);
+	char	*msg;
+
+	switch(error)
+	{
+		case ERR_UNK_OPTION :	msg = "Invalid option!\n"; break;
+		case ERR_IF_OPEN	:	msg = "Input file not found or access denied!\n"; break;
+		case ERR_OF_OPEN	:	msg = "Invalid output file name or access denied!\n"; break;
+		default				:	msg = "Undefined error code!\n"; break;
+	}
+
+	if(fp)
+		fclose(fp);
+	if(isxp)
+		free(isxp);
+
+	printf("\nError: %s", msg);
+
 	exit(EXIT_FAILURE);
 }
 
 
 int main (int argc, char *argv[])
 {
-	char				arg, *ext = NULL, fname[FILENAME_MAX], options, status;
+	char				arg, *ext = NULL, fname[FILENAME_MAX], options, rt, status;
 	unsigned char		bank[16384], header[32];
 	int					*sig;
 	long				fsize;
-	FILE				*ifp = NULL, *ofp = NULL;
 
 	if(argc < 2)
 		usage();
@@ -55,40 +70,72 @@ int main (int argc, char *argv[])
 			{
 //				case 'c': options |= FLAG_; break;
 
-//				default: error_handler(ERR_UNK_OPTION); break;
+//				default: handler(ERR_UNK_OPTION); break;
 			}
 		}
 		else
 		{
-			if(ifp == NULL)
-			{
-				ext = strstr(argv[arg], ".isx");
-				if(ext)
-				{
-					ifp = fopen(argv[arg], "rb");
-					fseek(ifp, 0L, SEEK_END);
-					fsize = ftell(ifp);
-					rewind(ifp);
-					strcpy(ext, ".gb");
-					ofp = fopen(argv[arg], "wb");
-				}
-			}
+			ext = strstr(argv[arg], ".isx");
+			if(ext)
+				strcpy(fname, argv[arg]);
 		}
 	}
 
-	if(ifp == NULL || fsize <= 32 || ofp == NULL)
-		error_handler(ifp, ofp);
+	fp = fopen(fname, "rb");
+	if(fp == NULL)
+		handler(ERR_IF_OPEN);
+	fseek(fp, 0L, SEEK_END);
+	fsize = ftell(fp);
+	if(fsize <= ISX_HEADER)
+		handler();
+	rewind(fp);
+	isxp =  malloc(fsize);
+	if(isxp == NULL)
+		handler();
+	fread(isxp, fsize, 1, fp);
+	fclose(fp);
 
-	fread(header, sizeof(header), 1, ifp);
-	sig = (int *) header;
+	sig = (int *) isxp;
+	if(*sig != ISX_SIGNATURE)
+		handler();
 
-	if(*sig != 0x20585349)
-		error_handler(ifp, ofp);
+	ext = strstr(fname, ".isx");
+	strcpy(ext, ".gb");
+	fp = fopen(fname, "wb");
+	if(fp == NULL)
+		handler(ERR_OF_OPEN);
 
-
-	printf("%.32s\n", header);
+	printf("%.32s\n", (char *) isxp);
+	isxp += ISX_HEADER;
 	memset(bank, 0, sizeof(bank));
 
-	fclose(ifp);
-	fclose(ofp);
+	while(fsize > 0)
+	{
+		rt = getc(fp);
+		fsize--;
+
+		switch(rt)
+		{
+			case 0x01:
+				bnk = getc(fp);
+				fsize--;
+				break;
+			case 0x03:
+				break;
+			case 0x04:
+				break;
+			case 0x11:
+				break;
+			case 0x13:
+				break;
+			case 0x14:
+				break;
+			default:
+				printf("unknown record type\n");
+				handler();
+		}
+	}
+
+	free(isxp);
+	fclose(fp);
 }
--- isx2gb.h
+++ isx2gb.h
@@ -2,11 +2,24 @@
 #define ISX2GB_H
 
 #define VERSION 		1.0
+#define ISX_HEADER		32
+#define ISX_SIGNATURE	0x20585349
 
 enum {  NO_ERROR,
 		ERR_UNK_OPTION,
 		ERR_NOT_FOUND
 };
+
+#pragma pack(push, 1)
+
+typedef struct _RT01H {
+	char	bnk;
+	short	adr;
+	short	len;
+} RT01H;
+
+#pragma pack(pop)
+
 
 
 #endif /* ISX2GB_H */
